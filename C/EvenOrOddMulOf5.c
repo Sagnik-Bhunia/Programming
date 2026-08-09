b061dc105e61d89fada3029155c56c435a5563ef
Sagnik-Bhunia <sagnikb2005@gmail.com>
2026-08-09 12:57:29 +0530

    Update EvenOrOddMulOf5.c

--- C/EvenOrOddMulOf5.c
+++ C/EvenOrOddMulOf5.c
@@ -1,5 +1,15 @@
-#include <stdio.h>
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+#include<stdio.h>
+int main()
+{ int n;
+printf("Enter an integer");
+scanf("%d",&n);
+if(n%2==0)
+    printf("%d is even.\n",n);
+else
+    printf("%d is odd.\n",n);
+if(n%5==0)
+    printf("%d is a multiple of 5");
+else
+    printf("%d is not a multiple of 5.",n);
+return 0;
 }
